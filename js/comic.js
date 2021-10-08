4b39f7aed33b960e7af882834cda8e13cfffecdc
Glitch (incandescent-broken-wallflower) <none>
2021-10-08 14:12:14 +0000

    💶🍈 Checkpoint ./js/comic.js:35297843/3448 ./index.html:35297843/59

---
+++ js/comic.js
@@ -0,0 +1,95 @@
+AFRAME.registerComponent("comic", {
+  init: function () {
+    this.placesContainer = this.el;   
+    this.createCards();
+  },
+
+  createCards: function () {
+    const thumbNailsRef = [
+      {
+        id: "iron-man",
+        title: "Iron Man",
+        url: "./thumbNails/iron-man.jpg",
+      },
+       {
+        id: "black-widow",
+        title: "Black Widow",
+        url: "./thumbNails/black-widow.jpg",
+      },
+
+       {
+        id: "spider-man",
+        title: "Spider Man",
+        url: "./thumbNails/spider-man.jpg",
+      },
+       {
+        id: "loki",
+        title: "Loki",
+        url: "./thumbNails/loki.jpg",
+      },
+    ];
+    
+    let prevoiusXPosition = -60;
+
+    for (var item of thumbNailsRef) {
+      const posX = prevoiusXPosition + 25;
+      const posY = 10;
+      const posZ = -40;
+      const position = { x: posX, y: posY, z: posZ };
+      prevoiusXPosition = posX;
+
+      // Border Element
+      const borderEl = this.createBorder(position, item.id);
+
+      // Thumbnail Element
+      const thumbNail = this.createThumbNail(item);
+      borderEl.appendChild(thumbNail);
+
+      // Title Text Element
+      const titleEl = this.createTitleEl(position, item);
+      borderEl.appendChild(titleEl);
+
+      this.placesContainer.appendChild(borderEl);
+    }
+  },
+  createBorder: function (position, id) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("id", id);
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("geometry", {
+      primitive: "box",
+    });
+    entityEl.setAttribute("position", position);
+    entityEl.setAttribute("material", {
+      color: "#0077CC",
+      opacity: 1,
+    });
+
+    return entityEl;
+  },
+  createThumbNail: function (item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("geometry", {
+      primitive: "box",
+    });
+    entityEl.setAttribute("material", { src: item.url });
+
+    return entityEl;
+  },
+  createTitleEl: function (position, item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("text", {
+      font: "exo2bold",
+      align: "center",
+      width: 70,
+      color: "#e65100",
+      value: item.title,
+    });
+    const elPosition = position;
+    elPosition.y = -20;
+    entityEl.setAttribute("position", elPosition);
+    entityEl.setAttribute("visible", true);
+    return entityEl;
+  },
+});
